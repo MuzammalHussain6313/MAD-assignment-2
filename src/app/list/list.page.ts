1a3194316a7e894d6ffac6254f67bc990d7f1d78
MuzammalHussain6313 <51987198+MuzammalHussain6313@users.noreply.github.com>
2019-10-30 14:26:30 +0500

    read data from remote API

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ListService} from '../list.service';
 import {Router} from '@angular/router';
+import {NavController} from '@ionic/angular';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -8,20 +11,38 @@ import {Router} from '@angular/router';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
-  constructor(private router: Router, private listService: ListService) {
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.data = this.http.get('https://test-node-api-test.herokuapp.com/students/getStudents');
+    this.data.subscribe(data => {
+      this.result = data;
+    });
   }
-  chepters = [];
-  ngOnInit() {
-    this.chepters = this.listService.getSUrahList();
-  }
-  changeUrl(item: any) {
-    const id = item.id.substring(0, 3);
-    const url = `list/${id}`;
-    this.router.navigateByUrl(url);
-}
 
-  addSurah() {
-    this.router.navigate(['add-surah']);
+  result: any = [];
+  data: Observable<any>;
+  clicked() {
+    this.data = this.http.get('https://test-node-api-test.herokuapp.com/students/getStudents');
+    this.data.subscribe(data => {
+      console.log(data);
+    });
   }
+
+  ngOnInit(): void {
+  }
+//   private selectedItem: any;
+//   constructor(private router: Router, private listService: ListService) {
+//   }
+//   chepters = [];
+//   ngOnInit() {
+//     this.chepters = this.listService.getSUrahList();
+//   }
+//   changeUrl(item: any) {
+//     const id = item.id.substring(0, 3);
+//     const url = `list/${id}`;
+//     this.router.navigateByUrl(url);
+// }
+//
+//   addSurah() {
+//     this.router.navigate(['add-surah']);
+//   }
 }
